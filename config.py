61ff2e23feb16f5a65edd31a7eae420f9fb1df8f
Noorfadilah Prayunto <nurfadilah2563@gmail.com>
2025-10-08 15:07:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 MODEL = os.getenv("MODEL")
 assert MODEL, "Set MODEL in .env (e.g., groq/openai/gpt-oss-120b)"
+
+EMBED_MODEL = os.getenv("EMBED_MODEL")
+assert MODEL, "Set EMBED_MODEL in .env (e.g., groq/openai/gpt-oss-120b)"
